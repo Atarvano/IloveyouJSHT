96c93a37f5fbee6a9f23b62522ad6dc97447fb41
Atarvano <vanheldyirvan@gmail.com>
2025-11-20 21:22:00 +0700

    feat: add extensive Data Structures & Algorithms content, advanced JavaScript topic exercises, and update project roadmap.

--- 01-Dasar-JavaScript/latihan_dasar.js
+++ 01-Dasar-JavaScript/latihan_dasar.js
@@ -2,19 +2,24 @@
 
 // 1. Buat variabel 'const' bernama 'negara' dan isi dengan "Indonesia".
 // Tulis kodemu di bawah ini:
+const negara = "indonesia";
 
 
 // 2. Buat variabel 'let' bernama 'kota' dan isi dengan nama kotamu.
 // Tulis kodemu di bawah ini:
-
+let kota = "batam";
 
 // 3. Ganti isi variabel 'kota' menjadi "Jakarta" (atau kota lain).
 // Tulis kodemu di bawah ini:
+kota = 'jambi';
 
 
 // 4. Buat variabel 'tahun' dengan tipe data Number, isi dengan tahun sekarang.
 // Tulis kodemu di bawah ini:
+let tahun = 2025;
 
 
 // 5. Cetak semua variabel di atas menggunakan console.log
 // Contoh: console.log(negara, kota, tahun);
+console.log(negara, kota, tahun);
+
