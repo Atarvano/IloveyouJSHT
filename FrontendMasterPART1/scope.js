972ed096ac20e0d02b8300071d345cdaceba79d2
Atarvano <vanheldyirvan@gmail.com>
2025-11-27 17:23:26 +0700

    Return greeting from sayHello and log it

--- FrontendMasterPART1/scope.js
+++ FrontendMasterPART1/scope.js
@@ -4,9 +4,9 @@ let age = 30;
 function sayHello() {
   nama = "Jane Doe";
   age = 25;
-  console.log("Hello, " + nama + " and I am " + age + " years old.");
+  return `Hello, ${nama} and I am ${age} years old.`;
 }
 
-sayHello();
+console.log(sayHello());
 
 console.log(`Hello, ${nama} and I am ${age} years old.`);
